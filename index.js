35350534564f0c6ac6d5dbef9ab910048dfac6d2
srujan-00 <srujanreddy658@gmail.com>
2023-01-04 14:26:01 +0530

    add controller, view, route for home; home contains a form which inputs task information in the ui(without functionality)

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-
+app.use('/', require('./routes'));
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
 app.listen(port, function (err) {
     if (err) {
-        console.log(`Oops! error: ${err} occured while trying to run the server`);
+        console.log(`Oops! error: ${err}  occured while trying to run the server`);
         return;
     }
     console.log(`The server is succesfully running on port: ${port}`);
